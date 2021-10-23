fb719352723e2795e891a77ac643750d8ab60ea0
Mat Richardson <m.richardson.home@googlemail.com>
2021-10-23 21:01:17 +0100

    Day 2 - Initial Commit

---
+++ Day 2.py
@@ -0,0 +1,15 @@
+def daytwo(a):
+    n = [] # this will store the new array
+    calc = 1 # this will store the intermediate calculated value for each pass through the array.  Start with 1 as will be used to multiply items
+
+    # iterate through the items in the array
+    for count1, value1 in enumerate(a):
+        for count2, value2 in enumerate(a):
+            if count1 != count2: #Check we aren't handling the current item in the array
+                calc = calc * value2
+        n.append(calc)
+        calc = 1
+
+    print(n)
+
+daytwo([1,2,3,4,5])
